5c17c7231d516c36144b3d78ac68bd8bc42aed4c
Dianka24336 <dianka24336@gmail.com>
2023-07-17 18:21:11 +0300

    corrected task-07

--- js/task-07.js
+++ js/task-07.js
@@ -1,7 +1,7 @@
 const inputRef = document.getElementById('font-size-control');
 const spanRef = document.getElementById('text');
 
-inputRef.addEventListener('click', onChangeFontCick);
+inputRef.addEventListener('input', onChangeFontCick);
 
 function onChangeFontCick() {
     spanRef.style.fontSize = `${inputRef.value}px`; //inputRef.value +'px'
